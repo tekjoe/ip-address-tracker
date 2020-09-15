3c6ecf55cb2dfeaf2b5cb4c6089f0efbec26daf4
tekjoe <ramirez.josephh@gmail.com>
2020-09-15 10:25:57 -0500

    worked on desktop styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,23 @@
 const form = document.querySelector(".form");
-const ipAddress = document.getElementById("ipAddress");
+const ipInput = document.getElementById("ipInput");
 const skeletons = document.querySelectorAll(".skeleton");
 
-const ip = document.getElementById("ip");
+const ipAddress = document.getElementById("ip");
 const city = document.getElementById("location");
-const timezone = document.getElementById("timezone");
+const tz = document.getElementById("timezone");
 const isp = document.getElementById("isp");
-
-const map = L.map("map", {
-  zoomControl: false,
-  center: [42.990716, -87.900571],
-  zoom: 15,
-});
 
 const accessToken =
   "pk.eyJ1IjoidGVram9lIiwiYSI6ImNrNTRmam1oMzBpd2YzZXBuNjBndDc2ZnUifQ.wjFDugqNW99oTEt_s8S7sA";
 
 const ipToken = "97463b4a4a65f7";
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  fetch(`http://ipinfo.io/${ipAddress.value}?token=97463b4a4a65f7`)
-    .then((res) => res.json())
-    .then((data) => {
-      const { loc } = data;
-      const coords = loc.split(",");
-      skeletons.forEach((skeleton) => (skeleton.style.display = "none"));
-      city.innerText = data.city;
-      ip.innerText = data.ip;
-      timezone.innerText = data.timezone;
-      isp.innerText = data.org;
-      L.marker(coords).addTo(map);
-      map.flyTo(coords);
-    });
+const ipRegex = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/;
+
+const map = L.map("map", {
+  zoomControl: false,
+  center: [42.990716, -87.900571],
+  zoom: 15,
 });
 
 L.tileLayer(
@@ -46,3 +31,49 @@ L.tileLayer(
     accessToken,
   }
 ).addTo(map);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  validateInput();
+};
+
+const validateInput = () => {
+  if (ipRegex.test(ipInput.value)) {
+    fetch(`http://ipinfo.io/${ipInput.value}?token=97463b4a4a65f7`)
+      .then((res) => res.json())
+      .then((data) => {
+        updateLayout(data);
+        const { loc } = data;
+        const coords = loc.split(",");
+        return coords;
+      })
+      .then((coords) => {
+        L.marker(coords).addTo(map);
+        map.flyTo(coords);
+      });
+  } else {
+    fetch(`http://ipinfo.io?token=97463b4a4a65f7`)
+      .then((res) => res.json())
+      .then((data) => {
+        updateLayout(data);
+        const { loc } = data;
+        const coords = loc.split(",");
+        return coords;
+      })
+      .then((coords) => {
+        L.marker(coords).addTo(map);
+        map.flyTo(coords);
+      });
+  }
+};
+
+const updateLayout = (data) => {
+  const { city: cityName, ip, timezone, org } = data;
+  skeletons.forEach((skeleton) => (skeleton.style.display = "none"));
+  city.innerText = cityName;
+  ipAddress.innerText = ip;
+  tz.innerText = timezone;
+  isp.innerText = org;
+};
+
+form.addEventListener("submit", handleSubmit);
